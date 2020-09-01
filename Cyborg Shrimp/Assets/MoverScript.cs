e3ebda5f32448a7e6f3fcee1efe046ada8ff376e
Christopher <10642047@my.uvu.edu>
2020-09-01 14:52:01 -0600

    Added MoverScript

---
+++ Cyborg Shrimp/Assets/MoverScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(message:"Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// Block has curly Brackets {}
