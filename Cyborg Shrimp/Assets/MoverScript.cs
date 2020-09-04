05883d10e4680d19bc86cfa53da1352310f4cf4a
Christopher <10642047@my.uvu.edu>
2020-09-04 16:38:17 -0600

    Sport Basketball Update

--- Cyborg Shrimp/Assets/MoverScript.cs
+++ Cyborg Shrimp/Assets/MoverScript.cs
@@ -1,24 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    public float speed = 3f;
-    public float x, y, z;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(message:"Hello World!");
-    }
+    public float speed = 5f;
+    public int score = 100;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        x = speed*Input.GetAxis("Horizontal")*Time.deltaTime;
-        y = speed*Input.GetAxis("Vertical")*Time.deltaTime;
-        transform.Translate(x, y, z);
+        var vInput = speed *Time.deltaTime* Input.GetAxis("Vertical");
+        var hInput= speed *Time.deltaTime* Input.GetAxis("Horizontal");
+        
+        transform.Translate(hInput,vInput,0);
     }
 }
 
-// Block has curly Brackets {}
