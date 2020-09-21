68b01fdcb1b951f3ece83aa422d824447bb1fb57
Christopher <10642047@my.uvu.edu>
2020-09-21 17:43:13 -0600

    I fixed the plane so it did not go backward and responded to user input
    You can now push up and down to make the plane fly up and down and now it goes forward

--- Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10.0f;
     public float rotationSpeed;
     public float verticalInput;
 
@@ -21,9 +21,9 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
