65cfa9288fef6ecc85ba67b074a709c3bd44402f
Christopher <10642047@my.uvu.edu>
2020-09-21 17:07:02 -0600

    Added turnSpeed float variable to be able to turn

--- Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
+++ Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         // We'll move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
