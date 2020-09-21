62f0373957da0dfb2efc01a145f9fc792be7d9be
Christopher <10642047@my.uvu.edu>
2020-09-21 17:12:19 -0600

    Updated Script to get horizontal input.

--- Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
+++ Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
     public float turnSpeed;
+
+    public float horozontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horozontalInput = Input.GetAxis("Horizontal");
         // We'll move the vehicle forward
         //transform.Translate(0, 0, 1);
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horozontalInput);
     }
 }
