b4da8e74333635d8ee9dd3d05456b323b3aa3b5a
Christopher <10642047@my.uvu.edu>
2020-09-21 17:01:57 -0600

    Changed the play-mode tint

--- Prototype 1 (2018.4 Editor)/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1 (2018.4 Editor)/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-
     private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
