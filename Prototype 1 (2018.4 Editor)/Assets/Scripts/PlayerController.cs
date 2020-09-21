4bd3e64cbba0d210b5eb8b7a234977e34b71605b
Christopher <10642047@my.uvu.edu>
2020-09-21 17:14:52 -0600

    Updated Script to get vertical input.

--- Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
+++ Prototype 1 (2018.4 Editor)/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
     public float turnSpeed;
-
     public float horozontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +18,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horozontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // We'll move the vehicle forward
         //transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horozontalInput);
     }
 }
