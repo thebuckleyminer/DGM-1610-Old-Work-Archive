63c68109ca2c1f15454fec5cda16fb78188237da
Christopher <10642047@my.uvu.edu>
2020-09-22 13:36:39 -0600

    DestroyOutOfBounds Script and changed the prefab through override

---
+++ Create with Code - Prototype 2 - Feed the Animals/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float zMaxValue = 30.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > zMaxValue)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
