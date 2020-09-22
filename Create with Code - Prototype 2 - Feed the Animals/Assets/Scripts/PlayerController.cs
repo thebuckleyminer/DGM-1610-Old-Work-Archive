6b63ae9abfbe2da1c43b9dfcda22ea99429531ac
Christopher <10642047@my.uvu.edu>
2020-09-22 13:11:53 -0600

    Established the xRange to limit X axis movement on left and right sides

--- Create with Code - Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
+++ Create with Code - Prototype 2 - Feed the Animals/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horosontalInput;
     public float speed = 10.0f;
+    public float xAxisRange = 15.0f;
+     
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xAxisRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xAxisRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xAxisRange)
+        {
+            transform.position = new Vector3(xAxisRange, transform.position.y, transform.position.z);
         }
         horosontalInput = Input.GetAxis("Horizontal");
         transform.Translate(speed * Time.deltaTime * horosontalInput * Vector3.right);
