a986cf986b265b7ca1891b00ddcb1215774a5c92
Christopher <10642047@my.uvu.edu>
2020-09-22 17:21:37 -0600

    Finished Challenge and Bonus challenges

--- Create With Code - Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create With Code - Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,21 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float lastLaunch;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (Time.time-1-lastLaunch>0)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                Debug.Log(lastLaunch);
+                lastLaunch = Time.time;
+                Debug.Log(lastLaunch);
+                
+            }
         }
     }
 }
--- Create With Code - Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Create With Code - Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -8,15 +8,15 @@ public class SpawnManagerX : MonoBehaviour
 
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnPosY = 25;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(1,4));
     }
 
     // Spawn random ball at random x position at top of play area
@@ -26,7 +26,7 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[Random.Range(0,3)], spawnPos, ballPrefabs[Random.Range(0,3)].transform.rotation);
     }
 
 }
