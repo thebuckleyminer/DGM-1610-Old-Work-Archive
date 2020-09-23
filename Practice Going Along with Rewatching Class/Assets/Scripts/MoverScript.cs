21b4ec6deec9beb93955cc5bbbfaf0779bd35a2c
Christopher <10642047@my.uvu.edu>
2020-09-23 14:18:50 -0600

    Switched to a 2D project

--- Practice Going Along with Rewatching Class/Assets/Scripts/MoverScript.cs
+++ Practice Going Along with Rewatching Class/Assets/Scripts/MoverScript.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class MoverScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Public if you want it to be VISIBLE inside the unity editor
+    //Private if you want it HIDDEN
+    public float speed = 10.0f;
+    public int score = 100;
+    //<Public or Private(Access Modifier)> <Data Type(ex. float int bool string)> <=> <what the variable is> <;>
+    //Variables first letter lower case, then camelcase after that.
+    
+    //Functions always start with an upper case letter
+    //Functions that do not return anything use the word void. Use "function" if returns something.
+    //Function name followed by ()
+    //And Function content is withing { }
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(vInput, hInput*Time.deltaTime, 0);
     }
 }
