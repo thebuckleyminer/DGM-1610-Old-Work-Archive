79931b076335eccd27c5aa5764de4fde2e7fe8c3
Christopher <10642047@my.uvu.edu>
2020-09-23 13:37:35 -0600

    Created a Sandbox Project to follow along with Previously Recorded class

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        print("Trigger");
-    }
+   private void Update()
+   {
+      transform.Rotate(0,.1f,0);
+   }
 }
