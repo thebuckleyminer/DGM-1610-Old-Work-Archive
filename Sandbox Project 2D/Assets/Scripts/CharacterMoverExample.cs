e19f356b96ab5ffd17e7cbd3e973000c67c0fffa
Christopher <10642047@my.uvu.edu>
2020-09-24 10:08:34 -0600

    Following 9-22-2020 Class added CharacterMoverExample Script

---
+++ Sandbox Project 2D/Assets/Scripts/CharacterMoverExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMoverExamnple : MonoBehaviour
+{
+    public CharacterController controller;
+    public Vector3 moveDirection;
+
+    private void Update()
+    {
+        controller.Move(moveDirection);
+    }
+}
