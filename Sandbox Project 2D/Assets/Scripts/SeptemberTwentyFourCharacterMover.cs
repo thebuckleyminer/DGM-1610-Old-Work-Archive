14c4576e8f18695cee9b54ff598bf569a46b3238
Christopher <10642047@my.uvu.edu>
2020-09-24 14:51:01 -0600

    Created SeptemberTwentyFourCharacterMover Script following class

---
+++ Sandbox Project 2D/Assets/Scripts/SeptemberTwentyFourCharacterMover.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class SeptemberTwentyFourCharacterMover : MonoBehaviour
+{
+   public CharacterController controller;
+   private Vector3 moveDirection;
+
+   private void Update()
+   {
+      moveDirection.Set(5f, 0, 0);
+      controller.Move(moveDirection * Time.deltaTime);
+   }
+   
+}
