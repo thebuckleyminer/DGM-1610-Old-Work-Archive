0251458705bcbad4ed37c7983534c93c7af4b868
Christopher <10642047@my.uvu.edu>
2020-09-30 16:30:24 -0600

    Finished Tuesday Class

--- Cyborg Shrimp 2/Assets/Scripts/Destroyer.cs
+++ Cyborg Shrimp 2/Assets/Scripts/Destroyer.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Destroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(other.gameObject);
     }
 }
