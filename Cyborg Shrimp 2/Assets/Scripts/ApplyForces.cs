ffb2b85a34184d26a99915ede9417a7749ea2718
Christopher <10642047@my.uvu.edu>
2020-09-30 15:09:30 -0600

    Created the ApplyForces script And click makes it jump

---
+++ Cyborg Shrimp 2/Assets/Scripts/ApplyForces.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ApplyForces : MonoBehaviour
+{
+    public Rigidbody rBody;
+    public float force = 30;
+    
+
+    void OnMouseDown()
+    {
+        rBody.AddForce(0,force,0);
+    }
+
+
+    void Update()
+    {
+        //rBody.AddForce(0,force,0); 
+    }
+}
