c010e93a3b5c1e0b9593a91fbb7891b07411ae36
Christopher <10642047@my.uvu.edu>
2020-09-30 14:50:05 -0600

    Made a new project to follow along with 9-29 class

--- Sandbox Project 2D/Assets/Scripts/SeptemberTwentyFourCharacterMover.cs
+++ Sandbox Project 2D/Assets/Scripts/SeptemberTwentyFourCharacterMover.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 public class SeptemberTwentyFourCharacterMover : MonoBehaviour
 {
    public CharacterController controller;
+   public float moveSpeed = 5f;
+   
+   
    private Vector3 moveDirection;
 
    private void Update()
    {
-      moveDirection.Set(5f, 0, 0);
+      var moveSpeedInput = moveSpeed * Input.GetAxis("Horizontal");
+         
+      moveDirection.Set(moveSpeedInput, 0, 0);
+
+      var movement = moveDirection * Time.deltaTime;
       controller.Move(moveDirection * Time.deltaTime);
    }
    
