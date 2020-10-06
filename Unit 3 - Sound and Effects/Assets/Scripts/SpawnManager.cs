181f0cc27845c1f9b714ec3be1142a4507112641
Christopher <10642047@my.uvu.edu>
2020-10-05 22:29:42 -0600

    Spawn at time intervals, Lock rigidbody rotation

--- Unit 3 - Sound and Effects/Assets/Scripts/SpawnManager.cs
+++ Unit 3 - Sound and Effects/Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,23 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2.0f;
+
+    private float repeatRate = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObsticle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObsticle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
