8c38b1de956a82e7d84cecaece0d6d4f6f45fbff
Christopher <10642047@my.uvu.edu>
2020-10-08 18:24:17 -0600

    Added my rotating code

---
+++ ParachuteGame/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public float horosontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horosontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Rotate(0, 0, 45 * horosontalInput);
+        Debug.Log(horosontalInput);
+        
+
+    }
+}
